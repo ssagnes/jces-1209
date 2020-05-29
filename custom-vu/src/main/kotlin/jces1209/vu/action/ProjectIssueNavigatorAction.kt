007c809a94e4193d0b7061004b6c72ded36583de
Nataliia Shavshyna <nshav@softserveinc.com>
2020-05-29 09:45:16 +0300

    JCSP-419: review comments fixes

--- custom-vu/src/main/kotlin/jces1209/vu/action/ProjectIssueNavigatorAction.kt
+++ custom-vu/src/main/kotlin/jces1209/vu/action/ProjectIssueNavigatorAction.kt
@@ -4,7 +4,6 @@ import com.atlassian.performance.tools.jiraactions.api.BROWSE_PROJECTS
 import com.atlassian.performance.tools.jiraactions.api.WebJira
 import com.atlassian.performance.tools.jiraactions.api.action.Action
 import com.atlassian.performance.tools.jiraactions.api.measure.ActionMeter
-import com.atlassian.performance.tools.jiraactions.api.memories.ProjectMemory
 import jces1209.vu.page.project.ProjectIssueNavigatorPage
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
@@ -12,7 +11,6 @@ import org.apache.logging.log4j.Logger
 class ProjectIssueNavigatorAction(
     private val jira: WebJira,
     private val meter: ActionMeter,
-    private val projectMemory: ProjectMemory,
     private val projectIssueNavigatorPage: ProjectIssueNavigatorPage,
     private val numberOfProject: Int
 
@@ -20,11 +18,7 @@ class ProjectIssueNavigatorAction(
     private val logger: Logger = LogManager.getLogger(this::class.java)
 
     override fun run() {
-        val project = projectMemory.recall()
-        if (project == null) {
-            logger.debug("Skipping Open project issue navigator...")
-            return
-        }
+
         meter.measure(BROWSE_PROJECTS) {
             jira.goToBrowseProjects(1).getProjects()
             projectIssueNavigatorPage.openProjectByIndex(numberOfProject)
