bca1bab53c505d01d452046128ef0014da74f451
Nataliia Shavshyna <nshavshyna@atlassian.com>
2020-06-01 16:46:33 +0300

    JCSP-419: review commit fixes

--- custom-vu/src/main/kotlin/jces1209/vu/page/project/CloudProjectIssueNavigatorPage.kt
+++ custom-vu/src/main/kotlin/jces1209/vu/page/project/CloudProjectIssueNavigatorPage.kt
@@ -1,19 +1,28 @@
 package jces1209.vu.page.project
 
+import com.atlassian.performance.tools.jiraactions.api.page.wait
+import jces1209.vu.page.CommonIssueNavigator
 import jces1209.vu.wait
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
+import java.time.Duration
 
 
 class CloudProjectIssueNavigatorPage(
     private val driver: WebDriver
-) : ProjectIssueNavigatorPage {
+) : ProjectIssueNavigatorPage, CommonIssueNavigator() {
     override fun openProjectByIndex(index: Int) {
         driver
             .wait(ExpectedConditions
                 .elementToBeClickable(By.xpath("(//*[@data-testid='Content']//a)[$index]")))
             .click()
         waitForProjectIssueNavigator(driver)
+    }
+
+    override fun waitForProjectIssueNavigator(driver: WebDriver) {
+        driver.wait(
+            Duration.ofSeconds(30),
+            expectedConditionsList())
     }
 }
--- custom-vu/src/main/kotlin/jces1209/vu/page/project/ProjectIssueNavigatorPage.kt
+++ custom-vu/src/main/kotlin/jces1209/vu/page/project/ProjectIssueNavigatorPage.kt
@@ -1,36 +1,14 @@
 package jces1209.vu.page.project
 
 import com.atlassian.performance.tools.jiraactions.api.page.wait
+import jces1209.vu.page.CommonIssueNavigator
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.support.ui.ExpectedConditions
 import java.time.Duration
 
-interface ProjectIssueNavigatorPage{
+interface ProjectIssueNavigatorPage {
 
     fun openProjectByIndex(index: Int)
-    fun waitForProjectIssueNavigator(driver: WebDriver) {
-
-        driver.wait(
-            Duration.ofSeconds(30),
-            ExpectedConditions.or(
-                ExpectedConditions.and(
-                    ExpectedConditions.or(
-                        ExpectedConditions.presenceOfElementLocated(By.cssSelector("ol.issue-list")),
-                        ExpectedConditions.presenceOfElementLocated(By.id("issuetable")),
-                        ExpectedConditions.presenceOfElementLocated(By.id("issue-content"))
-                    ),
-                    ExpectedConditions.or(
-                        ExpectedConditions.presenceOfElementLocated(By.id("jira-issue-header")),
-                        ExpectedConditions.presenceOfElementLocated(By.id("key-val"))
-                    ),
-                    ExpectedConditions.or(
-                        ExpectedConditions.presenceOfElementLocated(By.id("new-issue-body-container")),
-                        ExpectedConditions.presenceOfElementLocated(By.className("issue-body-content"))
-                    )
-                ),
-                ExpectedConditions.presenceOfElementLocated(By.className("no-results-hint")) // TODO is it too optimistic like in SearchServerFilter.waitForIssueNavigator ?
-            )
-        )
-    }
+    fun waitForProjectIssueNavigator(driver: WebDriver)
 }
